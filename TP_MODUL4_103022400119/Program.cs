8ccb7673b8dfa3b5cc2f397d36428d438dcf3a5f
Daniarthawiku <wikunugroho735@gmail.com>
2026-03-14 21:29:59 +0700

    Menambahkan class DoorMachine

--- TP_MODUL4_103022400119/Program.cs
+++ TP_MODUL4_103022400119/Program.cs
@@ -32,3 +32,38 @@ public class KodePos
         Console.WriteLine("Kode Pos " + kelurahan + " adalah: " + result);
     }
 }
+
+
+public class DoorMachine
+{
+    public enum DoorState { Terkunci, Terbuka };
+    private DoorState currentState = DoorState.Terkunci;
+
+    public DoorMachine()
+    {
+        Console.WriteLine("Pintu terkunci");
+    }
+    public void KunciPintu()
+    {
+            currentState = DoorState.Terkunci;
+            Console.WriteLine("Pintu terkunci");
+    }  
+    public void BukaPintu()
+    {
+        currentState = DoorState.Terbuka;
+        Console.WriteLine("Pintu tidak terkunci");
+    }
+       
+    public static void Main(string[] args)
+    {
+        DoorMachine pintu = new DoorMachine();
+        Console.WriteLine("\nMulai perubahan state");
+            
+        pintu.BukaPintu();  
+        pintu.KunciPintu();
+        pintu.KunciPintu(); 
+        pintu.BukaPintu();  
+    }
+}
+
+    
